60486c2aac00f13ffdcd6979e9f59bff9e85dff8
Dobromir Hristakiev <dobromir.hristakiev@ffwagency.com>
2021-03-18 15:29:14 +0200

    test

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /var/www/html/moodle/web
-yum install wget -y
-wget https://getcomposer.org/composer.phar
-php composer.phar install
+composer install
 php admin/cli purge_caches.php
